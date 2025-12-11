26ab0c5e82bc57c5f7e9aa1b79f2b1e217887b1e
Francisco Reyna <francisco.reyna@grupolufesa.com>
2025-12-11 12:10:37 -0600

    Feat: Adding loader styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import AppComponent from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import AppComponent from "./app/app.component";
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    const loader = document.getElementById("loader");
+    if (!loader) return;
+
+    const minTime = 1500; // 1.5s como antes
+
+    setTimeout(() => {
+      loader.classList.add("hidden");
+
+      // esperar el fade-out (1s) y quitarlo del DOM
+      setTimeout(() => loader.remove(), 1000);
+    }, minTime);
+  })
   .catch((err) => console.error(err));
